6363e6ec86d860941d67af49b18d50f3a2c2bf6d
Drushti <ramtekedrushti28@gmail.com>
2024-09-26 21:55:13 +0700

    Add files via upload

---
+++ accept.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<style>
+
+		body{
+			background-color: white;
+		}
+		fieldset{
+			text-align: center;
+			background-color: lightblue;
+			color: black;
+			margin-top: 30px;
+		}
+
+	</style>
+</head>
+<body>
+	
+		<fieldset>
+			<font color="black" size="5px">
+			You are registered successfully...
+		</font>
+		</fieldset>
+	
+</body>
+</html>
